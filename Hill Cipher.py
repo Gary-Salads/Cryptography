c1cf52b3edd8c0236eedca7b4f005b8dcce21c1d
Tayler Braga <bragatayler002@gmail.com>
2026-04-25 01:47:48 -0700

    added a way to create a key and made the UI look a little better

--- Hill Cipher.py
+++ Hill Cipher.py
@@ -2,15 +2,19 @@ import numpy as np
 
 Leave = False
 while Leave != True:
-    choice = input("q to exit p to play")
+    choice = input("q to exit p to play: ")
     if choice == "q":
         Leave = True
     elif choice == "p":
-        ExOne = int(input("What is X1:"))
-        ExTwo = int(input("What is X2:"))
-        A = np.array([[1, 2], [3, 4]])
+        ExOne = int(input("What is X1: "))
+        ExTwo = int(input("What is X2: "))
+        A11 = int(input("What is A11: "))
+        A12 = int(input("What is A12: "))
+        A21 = int(input("What is A21: "))
+        A22 = int(input("What is A22: "))
+        A = np.array([[A11, A12], [A21, A22]])
         B = np.array([[ExOne], [ExTwo]])
-        EorD = input("e for encryption d for decryption")
+        EorD = input("e for encryption d for decryption: ")
         if EorD == "e":
             ans = A @ B % 37
             print(ans)
